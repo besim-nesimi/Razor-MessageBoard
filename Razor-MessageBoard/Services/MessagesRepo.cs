5c63e4ada1daaacafc33de399953c6976c73e6b6
bestronaut <nesimibesim@outlook.com>
2023-03-09 11:43:28 +0100

    Create MessagesRepo.cs

---
+++ Razor-MessageBoard/Services/MessagesRepo.cs
@@ -0,0 +1,18 @@
+﻿using Razor_MessageBoard.Data;
+
+namespace Razor_MessageBoard.Services
+{ 
+    public class MessagesRepo
+    {
+        private readonly AppDbContext context;
+        public MessagesRepo(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task GetMessages()
+        {
+            return await context.Messages.ToList();
+        }
+    }
+}
